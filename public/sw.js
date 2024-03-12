f0cb453160685091b17c1b8debd359b059cb652e
marinaMob <mn@evamobi.com>
2024-03-12 16:39:49 +0700

    dfuhvr

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.3.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
